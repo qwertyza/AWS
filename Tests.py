755205d2c429b5bc2b1136e00498b4742c291397
qwertyza <t121211970@gmail.com>
2020-10-21 02:32:52 +0300

    added TESTS

---
+++ Tests.py
@@ -0,0 +1,15 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+
+    def test_add_integers(self):
+        """
+        Test random stuff
+        """
+        print('SHIIIIT BOY')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
