80909c1562269c56ce7163eb1eff77f82f450af2
qwertyza <t121211970@gmail.com>
2020-10-21 02:40:01 +0300

    added test stage to JenkinsFile3333

--- Tests.py
+++ Tests.py
@@ -1,14 +1,5 @@
 import unittest
 
-class TestCalc(unittest.TestCase):
-
-    def test_add_integers(self):
-        """
-        Test random stuff
-        """
-        print('SHIIIIT BOY')
-
-
 
 if __name__ == '__main__':
-    unittest.main()
+    print('DASDASDASDASDAS')
